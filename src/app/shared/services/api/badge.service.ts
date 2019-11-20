ef0affab42ae7e474d7ace74bbc96354415e12ed
ADIL LAHIB <lahib.adil@gmail.com>
2019-11-20 11:41:14 +0100

    A.L: correction badge.service & badgeType.service

--- src/app/shared/services/api/badge.service.ts
+++ src/app/shared/services/api/badge.service.ts
@@ -1,4 +1,4 @@
-import { BadgeType } from './../../models/BadgeType';
+import { Badge } from './../../models/badge';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { ProxyService } from './proxy.service';
@@ -8,22 +8,22 @@ import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class BadgeService {
-    controller = "badgeType";
+    controller = 'badge';
 
-    private badgeTypeList: BadgeType[] = [];
+    private badgeList: Badge[] = [];
 
-    badgeTypeListChanged = new Subject<BadgeType[]>();
+    badgeListChanged = new Subject<Badge[]>();
     constructor(private proxy: ProxyService, private toastr: ToastrService) {}
 
     private emitChanges() {
         this.findAll().subscribe(data => {
-            this.badgeTypeList = data;
-            this.badgeTypeListChanged.next(this.badgeTypeList);
+            this.badgeList = data;
+            this.badgeListChanged.next(this.badgeList);
         });
     }
 
-    findAll(): Observable<BadgeType[]> {
-        console.log("from driver service findAll");
+    findAll(): Observable<Badge[]> {
+        console.log('from driver service findAll');
         return this.proxy.findAll(this.controller);
     }
 
@@ -31,7 +31,7 @@ export class BadgeService {
         return this.proxy.find(this.controller, search);
     }
 
-    findById(id: number): Observable<BadgeType> {
+    findById(id: number): Observable<Badge> {
         // let TOKEN = this.token.computeToken('ems@ems.com', 'EMS', '77d2896c3eb544541f9389fe42651b0d');
         return this.proxy.findById(this.controller, id);
     }
@@ -52,55 +52,55 @@ export class BadgeService {
         return this.proxy.sizeSearch(this.controller, search);
     }
 
-    set(BadgeType: BadgeType): BadgeType {
-        this.proxy.set(this.controller, BadgeType).subscribe(
+    set(badge: Badge): Badge {
+        this.proxy.set(this.controller, badge).subscribe(
             data => {
                 this.emitChanges();
-                this.toastr.success("Item was saved successfully", "Save");
+                this.toastr.success('Item was saved successfully', 'Save');
                 return data;
             },
             error =>
                 this.toastr.error(
-                    "Item could not be saved successfully",
-                    "Save"
+                    'Item could not be saved successfully',
+                    'Save'
                 )
         );
         return null;
     }
 
-    setManually(BadgeType: BadgeType) {
-        return this.proxy.set(this.controller, BadgeType);
+    setManually(badge: Badge) {
+        return this.proxy.set(this.controller, badge);
     }
 
-    add(BadgeType: BadgeType): BadgeType {
-        this.proxy.add(this.controller, BadgeType).subscribe(
+    add(badge: Badge): Badge {
+        this.proxy.add(this.controller, badge).subscribe(
             data => {
                 this.emitChanges();
-                this.toastr.success("Item was saved successfully", "Save");
+                this.toastr.success('Item was saved successfully', 'Save');
                 return data;
             },
             error =>
                 this.toastr.error(
-                    "Item could not be saved successfully",
-                    "Save"
+                    'Item could not be saved successfully',
+                    'Save'
                 )
         );
         return null;
     }
 
-    delete(BadgeType: BadgeType) {
-        this.proxy.delete(this.controller,BadgeType.id).subscribe(
+    delete(badge: Badge) {
+        this.proxy.delete(this.controller, badge.id).subscribe(
             data => {
                 this.emitChanges();
                 this.toastr.success(
-                    "Elément supprimé avec succès",
-                    "Suppression"
+                    'Elément supprimé avec succès',
+                    'Suppression'
                 );
             },
             error =>
                 this.toastr.error(
-                    "Erreur de suppression" + error,
-                    "Suppression"
+                    'Erreur de suppression' + error,
+                    'Suppression'
                 )
         );
     }
--- src/app/shared/services/api/badgeType.service.ts
+++ src/app/shared/services/api/badgeType.service.ts
@@ -1,4 +1,4 @@
-import { Badge } from './../../models/badge';
+import { BadgeType } from './../../models/badge-type';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { ProxyService } from './proxy.service';
@@ -7,23 +7,23 @@ import { Subject } from 'rxjs/Subject';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
-export class BadgeService {
-    controller = "badges";
+export class BadgeTypeService {
+    controller = 'badgeTypes';
 
-    private badgeList: Badge[] = [];
+    private badgeTypeList: BadgeType[] = [];
 
-    badgeListChanged = new Subject<Badge[]>();
+    badgeTypeListChanged = new Subject<BadgeType[]>();
     constructor(private proxy: ProxyService, private toastr: ToastrService) {}
 
     private emitChanges() {
         this.findAll().subscribe(data => {
-            this.badgeList = data;
-            this.badgeListChanged.next(this.badgeList);
+            this.badgeTypeList = data;
+            this.badgeTypeListChanged.next(this.badgeTypeList);
         });
     }
 
-    findAll(): Observable<Badge[]> {
-        console.log("from driver service findAll");
+    findAll(): Observable<BadgeType[]> {
+        console.log('from driver service findAll');
         return this.proxy.findAll(this.controller);
     }
 
@@ -31,7 +31,7 @@ export class BadgeService {
         return this.proxy.find(this.controller, search);
     }
 
-    findById(id: number): Observable<Badge> {
+    findById(id: number): Observable<BadgeType> {
         // let TOKEN = this.token.computeToken('ems@ems.com', 'EMS', '77d2896c3eb544541f9389fe42651b0d');
         return this.proxy.findById(this.controller, id);
     }
@@ -52,55 +52,55 @@ export class BadgeService {
         return this.proxy.sizeSearch(this.controller, search);
     }
 
-    set(badge: Badge): Badge {
-        this.proxy.set(this.controller, badge).subscribe(
+    set(badgeType: BadgeType): BadgeType {
+        this.proxy.set(this.controller, badgeType).subscribe(
             data => {
                 this.emitChanges();
-                this.toastr.success("Item was saved successfully", "Save");
+                this.toastr.success('Item was saved successfully', 'Save');
                 return data;
             },
             error =>
                 this.toastr.error(
-                    "Item could not be saved successfully",
-                    "Save"
+                    'Item could not be saved successfully',
+                    'Save'
                 )
         );
         return null;
     }
 
-    setManually(badge: Badge) {
-        return this.proxy.set(this.controller, badge);
+    setManually(badgeType: BadgeType) {
+        return this.proxy.set(this.controller, badgeType);
     }
 
-    add(badge: Badge): Badge {
-        this.proxy.add(this.controller, badge).subscribe(
+    add(badgeType: BadgeType): BadgeType {
+        this.proxy.add(this.controller, badgeType).subscribe(
             data => {
                 this.emitChanges();
-                this.toastr.success("Item was saved successfully", "Save");
+                this.toastr.success('Item was saved successfully', 'Save');
                 return data;
             },
             error =>
                 this.toastr.error(
-                    "Item could not be saved successfully",
-                    "Save"
+                    'Item could not be saved successfully',
+                    'Save'
                 )
         );
         return null;
     }
 
-    delete(badge: Badge) {
-        this.proxy.delete(this.controller,badge.id).subscribe(
+    delete(badgeType: BadgeType) {
+        this.proxy.delete(this.controller, badgeType.id).subscribe(
             data => {
                 this.emitChanges();
                 this.toastr.success(
-                    "Elément supprimé avec succès",
-                    "Suppression"
+                    'Elément supprimé avec succès',
+                    'Suppression'
                 );
             },
             error =>
                 this.toastr.error(
-                    "Erreur de suppression" + error,
-                    "Suppression"
+                    'Erreur de suppression' + error,
+                    'Suppression'
                 )
         );
     }

cfe9d87fa04e270a1b9c27f042cf24b3693622c0
smekkaoui <smekkaoui@integrateur-si.com>
2020-02-12 15:19:58 +0100

    S.M :Modif

--- src/app/layouts/template/app-sidebar/app-sidebar.component.ts
+++ src/app/layouts/template/app-sidebar/app-sidebar.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './app-sidebar.component.html'
 })
 export class AppSidebarComponent implements OnInit {
-  ngOnInit() {}
+  ngOnInit() {
+
+
+      
+
+  }
 }

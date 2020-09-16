5648ffd8e41a127ae88c474d019515c960fc13f1
smekkaoui <smekkaoui@integrateur-si.com>
2020-09-16 14:27:22 +0100

    S.M: modif deploy

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,6 +1,6 @@
-//export const RESTGC_URL = 'http://51.75.250.147:8080/ems-rest-gestcoakymat/';
-//export const REST_URL = 'http://localhost:8087/';
-export const REST_URL = 'http://54.37.15.236:8080/tms-rest/';
 export const RESTGC_URL="";
+export const REST_URL = 'http://51.75.250.147:8080/tms-rest/';
+
+//export const REST_URL = 'http://localhost:8087/';
 export const CURRENT_USER = 'currentUser';
 export const LOGGED_IN = 'isLoggedin';
